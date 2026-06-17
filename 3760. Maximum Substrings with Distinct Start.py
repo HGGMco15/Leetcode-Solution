2f49e137362edce67390aa8f27a99de20f6168a6
HGGMco15 <ht900193@gmail.com>
2026-06-17 10:45:37 +0700

    Delete 3760. Maximum Substrings with Distinct Start.py

--- 3760. Maximum Substrings with Distinct Start.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxDistinct(self, s: str) -> int:
-        ls=[]
-        for x in range(len(s)):
-            if not s[x] in ls:
-                ls.append(s[x])
-        return len(ls)
