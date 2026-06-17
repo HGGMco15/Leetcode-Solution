17deb011d1602c7526485b11977043e91b23af37
HGGMco15 <ht900193@gmail.com>
2026-06-17 10:47:05 +0700

    Delete 1920. Build Array from Permutation.py

--- 1920. Build Array from Permutation.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        ls=[]
-        for x in range(len(nums)):
-            ls.append(nums[nums[x]])
-
-        return ls
