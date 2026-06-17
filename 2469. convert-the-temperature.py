75c4d0028931459ddb6250b0ec602700f6bf8f30
HGGMco15 <ht900193@gmail.com>
2026-06-17 10:49:06 +0700

    Delete 2469. convert-the-temperature.py

--- 2469. convert-the-temperature.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def convertTemperature(self, celsius: float) -> List[float]:
-        ls=[]
-        K=celsius+273.15
-        F=celsius*1.80+32.00
-        ls.append(K)
-        ls.append(F)
-        return ls
